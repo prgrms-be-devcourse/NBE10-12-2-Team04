da564bae6ab08dd006861f93fcc514035314a7b4
whitelinkblaze <whitelinkblaze@gmail.com>
2026-06-25 16:34:39 +0900

    chore: 경로 수정

---
+++ back/src/main/java/com/triptrace/global/app/AppConfig.java
@@ -0,0 +1,52 @@
+package com.triptrace.global.app;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
+//import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+//import org.springframework.security.crypto.password.PasswordEncoder;
+
+
+@Configuration
+public class AppConfig {
+    private static Environment environment;
+
+    @Autowired
+    public void setEnvironment(Environment environment) {
+        AppConfig.environment = environment;
+    }
+
+    public static boolean isDev() {
+        return environment.matchesProfiles("dev");
+    }
+
+    public static boolean isTest() {
+        return !environment.matchesProfiles("test");
+    }
+
+    public static boolean isProd() {
+        return environment.matchesProfiles("prod");
+    }
+
+    public static boolean isNotProd() {
+        return !isProd();
+    }
+
+//    @Bean
+//    public PasswordEncoder passwordEncoder() {
+//        return new BCryptPasswordEncoder();
+//    }
+
+//    @Getter
+//    private static ObjectMapper objectMapper;
+//
+//    @Autowired
+//    public void setObjectMapper(ObjectMapper objectMapper) {
+//        AppConfig.objectMapper = objectMapper;
+//    }
+
+//    @PostConstruct
+//    public void postConstruct() {
+//        Ut.json.objectMapper = objectMapper;
+//    }
+}
---
+++ back/src/main/java/com/triptrace/global/rq/Rq.java
@@ -0,0 +1,4 @@
+package com.triptrace.global.rq;
+
+public class Rq {
+}
