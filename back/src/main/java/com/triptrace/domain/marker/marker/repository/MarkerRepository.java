0726ce670b4440722b481c15f6b086e54521193e
hannahyang116 <hannayang116@gmail.com>
2026-06-26 19:39:00 +0900

    refactor: 게시글 마커 관계 일대일로 변경 #30

--- back/src/main/java/com/triptrace/domain/marker/marker/repository/MarkerRepository.java
+++ back/src/main/java/com/triptrace/domain/marker/marker/repository/MarkerRepository.java
@@ -3,9 +3,9 @@ package com.triptrace.domain.marker.marker.repository;
 import com.triptrace.domain.marker.marker.entity.Marker;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.List;
+import java.util.Optional;
 
 public interface MarkerRepository extends JpaRepository<Marker, Long> {
 
-    List<Marker> findByPostId(Long postId);
+    Optional<Marker> findByPostId(Long postId);
 }
