1a4bb799c6f13c7c04a1e6c53c77e7399fc3e947
bulmyung <bulmyun9@gmail.com>
2026-06-26 17:40:05 +0900

    feat: 좋아요 수 증가 테스트 코드 추가 #22

--- back/src/main/java/com/triptrace/domain/trip/tripLike/controller/ApiV1TripLikeController.java
+++ back/src/main/java/com/triptrace/domain/trip/tripLike/controller/ApiV1TripLikeController.java
@@ -4,9 +4,7 @@ import com.triptrace.domain.trip.tripLike.service.TripLikeService;
 import com.triptrace.global.rsData.RsData;
 import lombok.RequiredArgsConstructor;
 import org.springframework.transaction.annotation.Transactional;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/v1/trips")
@@ -17,7 +15,9 @@ public class ApiV1TripLikeController {
 
     @PostMapping("/{tripId}/likes")
     @Transactional
-    public RsData<Void> createLike(Long memberId, Long tripId) {
+    public RsData<Void> createLike(
+        @RequestParam Long memberId,
+        @PathVariable Long tripId) {
         tripLikeService.createLike(memberId, tripId);
 
         return new RsData<>(
