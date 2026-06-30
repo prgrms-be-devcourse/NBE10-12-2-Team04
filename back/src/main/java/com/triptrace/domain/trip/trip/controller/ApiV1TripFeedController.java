aa1e3b42781d397c0e4dd0beef0db71b07ad90cf
bulmyung <bulmyun9@gmail.com>
2026-06-30 15:13:17 +0900

    feat: 좋아요피드 컨트롤러 작성중

---
+++ back/src/main/java/com/triptrace/domain/trip/trip/controller/ApiV1TripFeedController.java
@@ -0,0 +1,38 @@
+package com.triptrace.domain.trip.trip.controller;
+
+import com.triptrace.domain.trip.trip.dto.TripResponse;
+import com.triptrace.domain.trip.trip.service.TripService;
+import com.triptrace.global.rsData.RsData;
+import jakarta.persistence.OrderBy;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.util.MultiValueMap;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/v1/feed/trips")
+@RequiredArgsConstructor
+public class ApiV1TripFeedController {
+    private final TripService tripService;
+
+    @GetMapping("/top-liked")
+    public RsData<List<TripResponse>> getLikedTop10(
+        @RequestParam Long tripId,
+        @RequestParam Long ownerId,
+        @RequestParam(value = "limit", defaultValue = "10") Long likeCount
+    ) {
+
+
+
+
+        return new RsData<> (
+            "200-1",
+            "좋아요 상위 10개 여행기 조회에 성공했습니다.",
+            tripService.findPublicTrips()
+        );
+    }
+
+
+}
