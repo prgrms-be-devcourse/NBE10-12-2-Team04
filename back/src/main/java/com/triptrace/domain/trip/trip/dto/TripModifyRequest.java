b748f3dffd4cfd8098f78a5db4d2dc96e6b2529d
hannahyang116 <hannayang116@gmail.com>
2026-06-30 10:21:40 +0900

    feat: 여행기에도 Valid 및 DTO 검증 추가 #49

--- back/src/main/java/com/triptrace/domain/trip/trip/dto/TripModifyRequest.java
+++ back/src/main/java/com/triptrace/domain/trip/trip/dto/TripModifyRequest.java
@@ -1,10 +1,17 @@
 package com.triptrace.domain.trip.trip.dto;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
 import java.time.LocalDateTime;
 
 public record TripModifyRequest(
+    @NotBlank
+    @Size(max = 100)
     String title,
+    @Size(max = 100)
     String country,
+    @Size(max = 100)
     String city,
     LocalDateTime startDate,
     LocalDateTime endDate,
