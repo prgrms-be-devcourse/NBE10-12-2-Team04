19d425336dd3e9dff38d61d2995050d849d23e83
bulmyung <bulmyun9@gmail.com>
2026-06-30 12:18:25 +0900

    chore: TripLikeService 테스트코드 작성중2

--- back/src/test/java/com/triptrace/domain/trip/tripLike/service/TripLikeServiceTest.java
+++ back/src/test/java/com/triptrace/domain/trip/tripLike/service/TripLikeServiceTest.java
@@ -1,14 +1,23 @@
 package com.triptrace.domain.trip.tripLike.service;
 
 import com.triptrace.domain.member.member.entity.Member;
+import com.triptrace.domain.member.member.entity.MemberStatus;
+import com.triptrace.domain.member.member.repository.MemberRepository;
 import com.triptrace.domain.trip.trip.entity.Trip;
+import com.triptrace.domain.trip.trip.repository.TripRepository;
+import com.triptrace.domain.trip.tripLike.entity.TripLike;
 import com.triptrace.global.exception.ServiceException;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
 
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
@@ -19,6 +28,32 @@ public class TripLikeServiceTest {
     private Member member = Mockito.mock(Member.class);
     private Trip trip = Mockito.mock(Trip.class);
     private TripLikeService tripLikeService = Mockito.mock(TripLikeService.class);
+    @Autowired
+    private MemberRepository memberRepository;
+    @Autowired
+    private TripRepository tripRepository;
+
+//    @BeforeEach
+//    public void setup() {
+//        Member member = memberRepository.save(new Member(
+//            "test@test.com",
+//            "member",
+//            "password1234",
+//            UUID.randomUUID().toString(),
+//            "imageUrl",
+//            MemberStatus.ACTIVE
+//        ));
+//
+//        Trip trip = tripRepository.save(new Trip(
+//            member,
+//            "test title",
+//            "test country",
+//            "test city",
+//            LocalDateTime.now().minusMonths(12),
+//            LocalDateTime.now().minusMonths(6),
+//            true
+//        ));
+//    }
 
     @Test
     @DisplayName("중복 좋아요를 repository없이 service단에서만 테스트")
@@ -34,5 +69,8 @@ public class TripLikeServiceTest {
     public void duplicateLikeTest2() throws Exception {
         tripLikeService.createLike(member.getId(), trip.getId());
         tripLikeService.createLike(member.getId(), trip.getId());
+        tripLikeService.createLike(member.getId(), trip.getId());
     }
 }
+
+
